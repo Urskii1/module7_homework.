d7bb7c306c51b216a6440b2de213e4fa8e7fb53f
Urskii1 <wowsirus336@mail.ru>
2024-07-02 10:59:11 +0300

    Update HW-1-7.js

--- HW-1-7.js
+++ HW-1-7.js
@@ -24,3 +24,34 @@ const obj ={
   console.log(result)
 
   //7.7.3
+
+const persons = [
+  {name: 'Dima', age: 18},
+  {name: 'Nikita', age: 15},
+  {name: 'Mark', age: 20}
+];
+
+const personOver18 = persons.filter(persons => persons.age >= 18);
+const personNameOver18 = persons.map(persons => persons.name)
+console.log(personOver18)
+console.log(personNameOver18)
+
+//7.7.4 
+function setFullName(obj, name) {
+  obj.fullName = name;
+};
+const setPersonFullName = setFullName.bind(null, persons);
+setPersonFullName("John Smith");
+console.log(persons.fullName);
+
+//7.7.5
+
+const arr = [5, 5, 2, 1, 3, 2, 4];
+function uniqueSorted(arr) {
+  const unique = arr.filter((item, index) => arr.indexOf(item) === index)
+  const sortAsc = unique.sort((a, b) => a - b);
+  return sortAsc;
+}
+
+const uniqueSortedArr = uniqueSorted(arr);
+console.log(uniqueSortedArr)
