c0dacb6f62e0eed69596b5099afe058e91ac6a59
Urskii1 <wowsirus336@mail.ru>
2024-07-02 10:37:48 +0300

    Update HW-1-7.js

--- HW-1-6.js
+++ HW-1-6.js
@@ -1,5 +1,11 @@
 //7.7.1
 
+const person = {Name: 'Sasha', Age: '20'}
+function printInfo(name, age) {
+  console.log(`Name: ${this.Name}, Age: ${this.Age}! `)
+}
+printInfo.call(person);
+
 //7.7.2
 
 const obj ={
